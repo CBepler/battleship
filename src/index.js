f270c1b34c836b13142a63abd62d60c6d13d7170
Christian Bepler <bepler135@gmail.com>
2024-08-04 14:14:59 -0400

    add ability to place ships && track turns && play local vs

--- src/index.js
+++ src/index.js
@@ -1,46 +1,100 @@
 import "./mycssNormalize.css"
 import "./style.css"
 import Player from "./player"
+import renderBoard from "./renderBoard";
+import setUpBoard from "./setupBoard";
+import getSpaceIndex from "./getSpaceIndex";
 
 const player1 = new Player;
 const player2 = new Player;
 
 let boards = document.querySelectorAll(".board");
 
+const directions = document.querySelector(".directions");
+
 let board1 = boards[0];
 let board2 = boards[1];
-board2.classList.toggle("no-click");
-board1.classList.toggle("no-click");
 
 const single = document.querySelector(".single");
 const versus = document.querySelector(".versus");
-const donePlacing = document.querySelector(".place-button");
 
-setUpBoard();
+const placeP1 = document.querySelector(".place-button-p1");
+const placeP2 = document.querySelector(".place-button-p2");
+
+setUpBoard(boards);
+
+let turn = 0;
+versus.addEventListener("click", () => {
+    turn = 0;
+    board1.addEventListener("click", placeShipState);
+    board1.classList.remove("no-click");
+    player1.board.resetBoard();
+    player2.board.resetBoard();
+    renderBoard(board1, player1, false);
+    renderBoard(board2, player2, false);
+    directions.textContent = "Player 1 place ships";
+    placeP1.classList.remove("hide");
+    placeP2.classList.add("hide");
+})
+
+placeP1.addEventListener("click", () => {
+    if(!player1.board.validShipState()) {
+        player1.board.resetBoard();
+        renderBoard(board1, player1, false);
+        directions.textContent = "Please place the proper ships. Player 1 place ships again";
+        return;
+    }
+    board1.removeEventListener("click", placeShipState);
+    board1.classList.add("no-click");
+    board2.addEventListener("click", placeShipState);
+    board2.classList.remove("no-click");
+    directions.textContent = "Player 2 place ships";
+    placeP1.classList.add("hide");
+    placeP2.classList.remove("hide");
+    renderBoard(board1, player1, true);
+
+})
+
+placeP2.addEventListener("click", () => {
+    if(!player2.board.validShipState()) {
+        player2.board.resetBoard();
+        renderBoard(board2, player2, false);
+        directions.textContent = "Please place the proper ships. Player 2 place ships again";
+        return;
+    }
+    board2.removeEventListener("click", placeShipState);
+    directions.textContent = "Pass computer to player 1 and press enter to play turn";
+    placeP2.classList.add("hide");
+    for(const board of boards) {
+        board.addEventListener("click", setplayState);
+    }
+    renderBoard(board1, player1, true);
+    renderBoard(board2, player2, true);
+})
+
+document.addEventListener("keydown", (e) => {
+    if(e.key === "Enter") {
+        if(turn % 2 === 0) {
+            renderBoard(board1, player1, false);
+            return;
+        }
+        if(turn % 2 === 1) {
+            renderBoard(board2, player2, false);
+            return;
+        }
+    }
+})
 
 single.addEventListener("click", () => {
-    for(const board of boards) {
-        board.addEventListener("click", placeShipState);
-    }
+    turn = 0;
+    board1.addEventListener("click", placeShipState);
+    board1.classList.remove("no-click");
+    player1.board.resetBoard();
+    player2.board.resetBoard();
+    renderBoard(board1, player1);
+    renderBoard(board2, player2);
+    directions.textContent = "Place Ships";
 })
-
-donePlacing.addEventListener("click", () => {
-    for(const board of boards) {
-        board.removeEventListener("click", placeShipState);
-        board.addEventListener("click", playState);
-    }
-    board1.classList.toggle("no-click");
-})
-
-function playState(e) {
-    let nextTurn = handleSpaceClick(e.target);
-    renderBoard(e.currentTarget, e.currentTarget.classList.contains("player1") ? player1 : player2)
-    if(nextTurn) {
-        board1.classList.toggle("no-click");
-        board2.classList.toggle("no-click");
-    }
-}
-
 
 let previousClick = [];
 function placeShipState(e) {
@@ -59,38 +113,8 @@ function placeShipState(e) {
     } catch {
 
     }
-    renderBoard(board, board.classList.contains("player1") ? player1 : player2);
+    renderBoard(board, board.classList.contains("player1") ? player1 : player2, false);
     previousClick = [];
-}
-
-function setUpBoard() {
-    for(let i = 0; i < player1.board.boardLength; i++) {
-        for(let j = 0; j < player1.board.boardLength; j++) {
-            for(const board of boards) {
-                const space = document.createElement("div");
-                space.classList.add("space");
-                board.appendChild(space);
-            }
-        }
-    }
-}
-
-function renderBoard(board, player) {
-    let children = Array.prototype.slice.call(board.children);
-    for(let i = 0; i < player.board.boardLength; i++) {
-        for(let j = 0; j < player.board.boardLength; j++) {
-            let color = "white";
-            if(player.board.board[i][j] === 1) {
-                color = "green";
-            } else if(player.board.board[i][j] === 2) {
-                color = "blue";
-            } else if (player.board.board[i][j] === 3) {
-                color = "red";
-            }
-            let index = i * player.board.boardLength + j
-            children[index].style.backgroundColor = color;
-        }
-    }
 }
 
 
@@ -101,20 +125,38 @@ function getPlayer(space) {
     return player2;
 }
 
-function getSpaceIndex(space) {
-    const children = Array.prototype.slice.call(space.parentElement.children);
-    const index = children.indexOf(space);
-    return [Math.floor(index / player1.board.boardLength), index % player1.board.boardLength];
-}
-
 function handleSpaceClick(space) {
     const player = getPlayer(space);
     const index = getSpaceIndex(space);
-    if(player.board.board[index[0]][index[1]] === 0) {
-        player.board.board[index[0]][index[1]] = 2;
-        return true;
-    } else if(player.board.board[index[0]][index[1]] === 1) {
-        player.board.board[index[0]][index[1]] = 3;
+    let endAttack = player.board.recieveAttack(index);
+    return endAttack;
+}
+
+function handleEndGame() {
+    if(player1.board.allSunk()) {
+        directions.textContent = "Player 2 Wins";
     }
-    return false;
+    if(player2.board.allSunk()) {
+        directions.textContent = "Player 1 Wins";
+    }
+    for(const board of boards) {
+        board.removeEventListener("click", setplayState);
+        board.classList.add("no-click");
+    }
+}
+
+function setplayState(e) {
+    let nextTurn = handleSpaceClick(e.target);
+    renderBoard(e.currentTarget, e.currentTarget.classList.contains("player1") ? player1 : player2, true)
+    if(player1.board.allSunk() || player2.board.allSunk()) {
+        handleEndGame();
+    }
+    if(nextTurn) {
+        renderBoard(board1, player1, true);
+        renderBoard(board2, player2, true);
+        board1.classList.toggle("no-click");
+        board2.classList.toggle("no-click");
+        turn++;
+        directions.textContent = "Pass computer to player " + ((turn % 2) === 0 ? 1 : 2) + " and press enter to play turn";
+    }
 }
